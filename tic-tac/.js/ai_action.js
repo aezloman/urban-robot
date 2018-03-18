4369e2f6f7b4963445bcf9530833c1425a5f27c7
Aezlo Man <aezloman@athelo.net>
2018-03-18 14:00:54 +0000

    Create ai_action.js

---
+++ tic-tac/.js/ai_action.js
@@ -0,0 +1,32 @@
+/*
+ * Constructs an action that the ai player could make
+ * @param pos [Number]: the cell position the ai would make its action in
+ * made that action
+ */
+var AIAction = function(pos) {
+
+    // public : the position on the board that the action would put the letter on
+    this.movePosition = pos;
+
+    //public : the minimax value of the state that the action leads to when applied
+    this.minimaxVal = 0;
+
+    /*
+     * public : applies the action to a state to get the next state
+     * @param state [State]: the state to apply the action to
+     * @return [State]: the next state
+     */
+    this.applyTo = function(state) {
+        var next = new State(state);
+
+        //put the letter on the board
+        next.board[this.movePosition] = state.turn;
+
+        if(state.turn === "O")
+            next.oMovesCount++;
+
+        next.advanceTurn();
+
+        return next;
+    }
+};
