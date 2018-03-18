1445e9900a5d97308d208e75d14b2086fa3f2c24
Aezlo Man <aezloman@athelo.net>
2018-03-18 14:05:06 +0000

    Create novice.js

---
+++ tic-tac/.js/novice.js
@@ -0,0 +1,7 @@
+var P = 40; //some probability in percent form
+if(Math.random()*100 <= P) {
+    // carry out the probable task with probability P
+}
+else {
+   // carry out the other probable task with probability 1 - P
+}
